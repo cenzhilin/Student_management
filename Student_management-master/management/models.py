15b9cceb34b452f19b77fa0c5f26fe68946c3d04
cenzhilin <690693258@qq.com>
2019-03-02 16:12:16 +0800

    改为python3 django2.0 数据库mysql
    改为python3 django2.0 数据库mysql

---
+++ Student_management-master/management/models.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    s_number = models.IntegerField(primary_key=True)
+    s_name = models.CharField(max_length=20)
+    sex = models.CharField(max_length=2)
+    subject = models.CharField(max_length=20)
+    grade = models.CharField(max_length=20)
+    ID_number = models.IntegerField()
+    native_place = models.CharField(max_length=30)
+
+class Lesson(models.Model):
+    l_number = models.IntegerField(primary_key=True)
+    l_name = models.CharField(max_length=30)
+    credit = models.CharField(max_length=6)
+    time = models.IntegerField()
+
+class Teacher(models.Model):
+    t_number = models.IntegerField(primary_key=True)
+    t_name = models.CharField(max_length=10)
+    t_pass = models.IntegerField(null=True)
+    t_lesson_id = models.IntegerField()
+    t_lesson_name = models.CharField(max_length=10)
+
+class Score(models.Model):
+    sName = models.CharField(max_length=10)
+    sNum = models.IntegerField()
+    lName = models.CharField(max_length=10)
+    lNum = models.IntegerField()
+    score = models.IntegerField()
+    lCredit = models.CharField(max_length=6)
+    lTime = models.IntegerField()
+
---
+++ Student_management-master/management/views.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from  .models import Student,Score,Lesson,Teacher
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+# Create your views here.
+def index(request):
+    return render(request, "login.html")
+
+def change(request):
+
+        lid = int(request.GET['lid'])
+        sid = int(request.GET['sid'])
+        sscore = request.POST.get("score", None)
+        Score.objects.filter(id=sid).update(score=sscore)
+        score = Score.objects.filter(lNum=lid)
+        teacher = Teacher.objects.get(t_lesson_id=lid)
+        return render(request, "teacher.html", {"teacher": teacher, "score": score})
+
+
+
+def login(request):
+    if request.method == "POST":
+        type = request.POST.get("fruit")
+        name = request.POST.get("username", None)
+        ps = request.POST.get("password", None)
+
+    if type == "1":
+
+        try:
+            s = Student.objects.get(s_number=name)
+        except Student.DoesNotExist:
+            return HttpResponseRedirect("/")
+        idnum = int(str(s.ID_number)[-6:])
+        if idnum == int(ps):
+            student = Student.objects.get(s_number=name)
+            score = Score.objects.filter(sNum=name)
+            return render(request, "student.html", {"student": student, "score": score})
+        else:
+            return HttpResponseRedirect("/")
+    elif type == "2":
+
+        try:
+            t = Teacher.objects.get(t_number=name)
+        except Teacher.DoesNotExist:
+            return HttpResponseRedirect("/")
+        tps = t.t_pass
+        if tps == int(ps):
+            teacher = Teacher.objects.get(t_number=name)
+            l_num = teacher.t_lesson_id
+            score = Score.objects.filter(lNum=l_num)
+            return render(request, "teacher.html", {"teacher": teacher, "score": score})
+        else:
+            return HttpResponseRedirect("/")
+    else:
+        return  HttpResponseRedirect("/admin/")
